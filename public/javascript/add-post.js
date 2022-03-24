cade94467b4bcd91afcd2453c774073baf280f43
snew430 <seanpnew@hotmail.com>
2022-03-23 21:23:11 -0400

    movie poster added

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -8,6 +8,7 @@ async function newFormHandler(event) {
     'textarea[name="post-text"]'
   ).value;
   const movie_rating = document.querySelectorAll(".fas").length;
+  const poster = document.querySelector("#movie-poster").getAttribute("src");
   const movie_id = document
     .querySelector("#movie-title")
     .getAttribute("data-id");
@@ -23,6 +24,7 @@ async function newFormHandler(event) {
       body: JSON.stringify({
         movie_id,
         title,
+        poster,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -56,9 +58,12 @@ document
 // Use the choose button from a movie and populate the info
 document.addEventListener("click", function (e) {
   if (e.target && e.target.className == "movieChoice") {
+    console.log(e.target.parentElement.childNodes);
     document.querySelector("#movie-title").innerText =
       e.target.parentElement.childNodes[0].textContent;
     const movie_id = e.target.parentElement.getAttribute("data-id");
     document.querySelector("#movie-title").setAttribute("data-id", movie_id);
+    const moviePoster = e.target.parentElement.childNodes[1].src;
+    document.querySelector("#movie-poster").setAttribute("src", moviePoster);
   }
 });
